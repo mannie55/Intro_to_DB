df2c18d567bd97598a1fcde0bb3f97f119673848
mannie55 <mannnie55@gmail.com>
2024-07-27 20:11:04 +0100

    fix: fixed issues with code

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
-SELECT COLUMN_NAME, DATA_TYPE 
-FROM information_schema.columns
-WHERE TABLE_SHEMA = 'alx_book_store'
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
 AND TABLE_NAME = 'Books';
