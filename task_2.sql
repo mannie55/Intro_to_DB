d8c66b8b96450d2e0099ec969d9026d9e63fa2a9
mannie55 <mannnie55@gmail.com>
2024-07-27 17:30:01 +0100

    fix: add email to Customers table

--- task_2.sql
+++ task_2.sql
@@ -18,6 +18,7 @@ CREATE TABLE IF NOT EXISTS Books (
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
 
