7534273fe0bc81351f12c7e9dfd88831a199cace
mannie55 <mannnie55@gmail.com>
2024-07-27 15:56:02 +0100

    create: MySQLServer.py script that creates a database, updated readme file

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydatabase = mysql.connector.connect(
+     host="localhost",
+    user="root",
+    password="Indespicableme11#",
+)
+cursor = mydatabase.cursor()
+try:
+    cursor.execute("CREATE DATABASE alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except Exception as e:
+    print(e)
+    
+cursor.close()
+mydatabase.close()
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Intro_to_DB
 Mysql Database repo, for completing Alx tasks
 alx_book_store.sql: A Magical Database for Your Dream Online Bookstore Adventure!<br>
+MySQLServer.py: Let's Build Your Database: Your Gateway to Data Adventure!<br>
